b5faf6df255efd1b2273f3f37e76f049fa1f4fd3
Joseph990 <jtb072@email.latech.edu>
2020-04-11 20:46:23 -0500

    Added the necessary modules and images

--- 132Project.py
+++ 132Project.py
@@ -1,1 +1,30 @@
+from skimage.measure import compare_ssim as ssim
+from skimage import data, img_as_float
+from picamera import Picamera
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+image0 = cv2.imread("zero.jpg")
+image1 = cv2.imread("One.jpg")
+image2 = cv2.imread("Two.jpg")
+image3 = cv2.imread("Three.jpg")
+image4 = cv2.imread("Four.jpg")
+image5 = cv2.imread("Five.jpg")
+image6 = cv2.imread("Six.jpg")
+image7 = cv2.imread("Seven.jpg")
+image8 = cv2.imread("Eight.jpg")
+image9 = cv2.imread("Nine.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
 
